48fbb7f25345c30234169e3b27c1a9ab81f39c6d
alex <alm_main@proton.me>
2023-08-03 16:24:24 +0000

    added nsec functionality

--- key_generator.py
+++ key_generator.py
@@ -1,6 +1,7 @@
 import os
 import binascii
 from secp256k1 import PrivateKey
+import bech32
 
 def generate_keypair():
     private_key = PrivateKey()
@@ -12,8 +13,23 @@ def keypair_to_hex(private_key, public_key):
     public_key_hex = binascii.hexlify(public_key.serialize()).decode()
     return private_key_hex, public_key_hex
 
+def encode_data(hrp, data):
+    # Custom encoding function to encode only the data part
+    data_int = bech32.convertbits(data, 8, 5)
+    return bech32.bech32_encode(hrp, data_int)
+
+def private_key_to_nsec(private_key_hex):
+    private_key_bytes = bytes.fromhex(private_key_hex)
+    nsec_hrp = "nsec"
+    nsec_version = 0
+    nsec_encoded = encode_data(nsec_hrp, private_key_bytes)
+    return nsec_encoded
+
 private_key, public_key = generate_keypair()
 private_key_hex, public_key_hex = keypair_to_hex(private_key, public_key)
 
 print("Private Key:", private_key_hex)
 print("Public Key:", public_key_hex)
+
+nsec_key = private_key_to_nsec(private_key_hex)
+print("NSEC Key:", nsec_key)
